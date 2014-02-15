fb1247176f69a9adc1898b0c78efe90c43de4dfd
hcfranck <hc_throwdown@hotmail.com>
2014-02-15 14:37:05 -0500

    Start over !! trop merdique..

--- Jedit.java
+++ Jedit.java
@@ -9,21 +9,6 @@ public class Jedit
    
    public static void main( String args[] )
    {
-	   Jedit app = new Jedit();
-	   PrintWriter writer; // Déclaration du flux d'écriture
-	   String nomFichier = args[3]
-	   {
-			writer = new Printwriter(
-						 new BufferedWriter(
-						     new FileWriter(nomFichier))); //Écrit dans le nom du fichier en 3eme argument
-							 
-			while(...) // while la chaine rentré est == a " " chaine vide
-			{
-				writer.println(nomFichier
-			}
-							 
 	   
-	   
-       //--app.editer( args );
    }
 }
