24beba70e33f3c2054e2228fcf17a6cf24255a72
hcfranck <hc_throwdown@hotmail.com>
2014-02-15 13:36:21 -0500

    Push cuz restarting laptop , but biggest turd ever

---
+++ Jedit.java
@@ -0,0 +1,29 @@
+import java.io.*;
+public class Jedit
+{
+	
+   public void editer( String args[] )
+   {
+      ...
+   }
+   
+   public static void main( String args[] )
+   {
+	   Jedit app = new Jedit();
+	   PrintWriter writer; // Déclaration du flux d'écriture
+	   String nomFichier = args[3]
+	   {
+			writer = new Printwriter(
+						 new BufferedWriter(
+						     new FileWriter(nomFichier))); //Écrit dans le nom du fichier en 3eme argument
+							 
+			while(...) // while la chaine rentré est == a " " chaine vide
+			{
+				writer.println(nomFichier
+			}
+							 
+	   
+	   
+       //--app.editer( args );
+   }
+}
