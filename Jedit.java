a0f5ff9f9f6ef80a773692e51a023a773a41b6a1
hcfranck <hc_throwdown@hotmail.com>
2014-02-15 21:39:11 -0500

    sadasdffdsf

--- Jedit.java
+++ Jedit.java
@@ -4,11 +4,33 @@ public class Jedit
 	
    public void editer( String args[] )
    {
-      ...
+      PrintWriter writer;
+	   String nomFichier
+	   		
+	   		if(args[2] == "-n")
+	    	nomFichier = args[3]; // Nom du Ficher est contenu dans l'argument 3 exemple java Jedit -n exemple.txt
+	   		else
+	   		nomFichier = args[2]; // Nom du Ficher est contenu dans l'argument 2 exemple java Jedit exemple.txt
+	   try
+	   {
+	   		writer = new PrintWriter(
+	   					new BufferedWriter(
+	   						new FileWriter(nomFicher)));
+	   					
+	   		if(nomFichier.exists())
+	   		{
+	   			System.out.println("Le fichier existe deja .")
+	   			System.out.println("Voulez-vous le remplacer? O(oui) N(non)")
+	   			
+	   		}
+	   		
+	   			
+	   }
    }
    
    public static void main( String args[] )
    {
-	   
+	  Jedit app = new Jedit();
+      app.editer( args );
    }
 }
