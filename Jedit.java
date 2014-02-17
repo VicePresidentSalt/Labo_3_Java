5761447bcf013097ac0aac5c6c4744efa64b36f0
Daren-Ken St-laurent <daren511@hotmail.com>
2014-02-17 10:55:01 -0500

    fini! missing ze francis part

--- Jedit.java
+++ Jedit.java
@@ -9,80 +9,124 @@ public class Jedit {
          System.out.println("Aucun Fichier créé / Aucun paramètres");
          onContinue = false;
       }
-      else if (args.length>2)
+      else if (args.length > 2)
       {
          System.out.println("Aucun Fichier créé / Trop de paramètres");
          onContinue = false;
       }
-      else if (args[0].equals("-n"))
+      else if (!args[0].equals("-n") && args.length == 2)
       {
          System.out.println("Aucun Fichier créé / Option invalide");
          onContinue = false;
       }
       if (onContinue) // nous avons ici l'assurance que le nombre de parametre est valide et que l'option demande si tel est le cas est -n
       {
-         String nomFichier
+         String nomFichier;
          File fichier;
-         PrintWriter writer;
-         BufferedReader Reader;
-         boolean pasFini = true;
-         String ligne;
 
          if (args.length == 2) //verification de ou est le nom de fichier
             nomFichier = args[1]; // Nom du Ficher est contenu dans l'argument 1 exemple java Jedit -n exemple.txt
          else
             nomFichier = args[0]; // Nom du Ficher est contenu dans l'argument 0 exemple java Jedit exemple.txt
 
-         fichier = new File(nomFichier)
+         fichier = new File(nomFichier);
          if (fichier.exists())
          {
             System.out.println("Le fichier existe deja .");
             System.out.println("Voulez-vous le remplacer? O(oui) N(non)");
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); // should be working now have to try
-            char reponse = (char)br.read();
-            if (reponse == 'n' || reponse == 'N')
+            try
             {
-               System.out.println("Le fichier ne sera pas modifié / Refus");
-            }
-            if (reponse == 'o' || reponse == 'O')
-            {
-               System.out.println("Le fichier sera modifié / Fichier écrasé");
+               BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); // should be working now have to try
+               char reponse = (char)br.read();
 
-               lireetecrire(args.length);
+               if (reponse == 'o' || reponse == 'O')
+               {
+                  System.out.println("Le fichier sera modifié / Fichier écrasé");
+
+                  lireetecrire(args.length, args);
+               }
+               else
+               {
+                  System.out.println("Refus / le fichier ne sera pas modifié / opération annulée");
+               }
+            }
+            catch (IOException ioe)
+            {
+               System.err.println(ioe);
+               System.exit(1);
             }
          }
          else
          {
-            lireetecrire(args.length);
+            lireetecrire(args.length, args);
          }
-            // pas fou mais jaime pas i think i have a better idea TBV
-         //if (args.length == 2)
-         //{
-         //   for (int i = 1; i < nomFichier.length; i++)
-         //   {
-         //      System.out.println(i + ":");//ligne de texte);
-         //   }
-         //}
-      }
-         //catch (IOException ioe)
-         //{
-         //   System.err.println(ioe);
-         //   System.exit(1);
-         //}
       }
    }
 
-   private void lireetecrire(int nbarguments)
+   private void lireetecrire(int nbarguments, String[] args)
    {
+      boolean numero;
+      String nomFichier;
+      if (nbarguments == 1)
+      {
+         numero = false;
+         nomFichier = args[0];
+      }
+      else
+      {
+         numero = true;
+         nomFichier = args[1];
+      }
+      try
+      {
+         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+         PrintWriter writer = new PrintWriter(
+            new BufferedWriter(
+            new FileWriter(nomFichier)));
+
+
+         int compteur = 1;
+         String ligne = reader.readLine();
+
+         while (!ligne.trim().isEmpty())
+         {
+            if (numero)
+            {
+               if (compteur > 1)
+               {
+                  writer.print("\n" + compteur + ": " + ligne);
+               }
+               else
+               {
+                  writer.print(compteur + ": " + ligne);
+               }
+               ++compteur;
+               ligne = reader.readLine();
+            }
+            else
+            {
+               writer.print(ligne);
+               ligne = "\n" + reader.readLine();
+            }
+         }
+
+         writer.close();
+      }
+      catch (IOException ioe)
+      {
+         System.err.println(ioe);
+         System.exit(1);
+      }
 
 
 
    }
 
-   public static void main( String args[] )
+   public static void main(String args[])
    {
       Jedit app = new Jedit();
       app.editer(args);
    }
 }
+
 
