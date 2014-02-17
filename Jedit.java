053408bb83b0fad0235446be9fd3dc2747d5961b
hcfranck <hc_throwdown@hotmail.com>
2014-02-17 11:07:55 -0500

    On n'a gagné

--- Jedit.java
+++ Jedit.java
@@ -4,21 +4,23 @@ public class Jedit {
    public void editer(String args[])
    {
       boolean onContinue = true;
+      // Gestion des erreurs
       if (args.length == 0)
       {
-         System.out.println("Aucun Fichier créé / Aucun paramètres");
+         System.out.println("Aucun Fichier cree / Aucun parametres");
          onContinue = false;
       }
       else if (args.length > 2)
       {
-         System.out.println("Aucun Fichier créé / Trop de paramètres");
+         System.out.println("Aucun Fichier cree / Trop de parametres");
          onContinue = false;
       }
       else if (!args[0].equals("-n") && args.length == 2)
       {
-         System.out.println("Aucun Fichier créé / Option invalide");
+         System.out.println("Aucun Fichier cree / Option invalide");
          onContinue = false;
       }
+
       if (onContinue) // nous avons ici l'assurance que le nombre de parametre est valide et que l'option demande si tel est le cas est -n
       {
          String nomFichier;
@@ -29,6 +31,7 @@ public class Jedit {
          else
             nomFichier = args[0]; // Nom du Ficher est contenu dans l'argument 0 exemple java Jedit exemple.txt
 
+         //Vérification si le fichier existe ou non
          fichier = new File(nomFichier);
          if (fichier.exists())
          {
@@ -39,15 +42,17 @@ public class Jedit {
                BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); // should be working now have to try
                char reponse = (char)br.read();
 
+               //Si existe écraser
                if (reponse == 'o' || reponse == 'O')
                {
-                  System.out.println("Le fichier sera modifié / Fichier écrasé");
+                  System.out.println("Le fichier sera modifie / Fichier ecrase");
 
                   lireetecrire(args.length, args);
                }
+               // Refus de remplacer
                else
                {
-                  System.out.println("Refus / le fichier ne sera pas modifié / opération annulée");
+                  System.out.println("Refus / le fichier ne sera pas modifie / opération annulee");
                }
             }
             catch (IOException ioe)
@@ -56,17 +61,19 @@ public class Jedit {
                System.exit(1);
             }
          }
+         // Si n'existe pas écrire
          else
          {
             lireetecrire(args.length, args);
          }
       }
    }
-
+   //Fonction de lecture et d'écriture
    private void lireetecrire(int nbarguments, String[] args)
    {
       boolean numero;
       String nomFichier;
+      // Évaluer le nombre d'argument si implementation de  # de ligne est requise
       if (nbarguments == 1)
       {
          numero = false;
@@ -79,23 +86,28 @@ public class Jedit {
       }
       try
       {
+         //Lecture & Écriture
          BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
          PrintWriter writer = new PrintWriter(
             new BufferedWriter(
             new FileWriter(nomFichier)));
 
-
+         //Compteur a 1 pour # de ligne
          int compteur = 1;
+         // Lecture de la ligne entré
          String ligne = reader.readLine();
-
+         //Pendant que la ligne lu n'est pas vide
          while (!ligne.trim().isEmpty())
          {
+            //Si # de ligne est requis
             if (numero)
             {
+               //1ere ligne avec #
                if (compteur > 1)
                {
                   writer.print("\n" + compteur + ": " + ligne);
                }
+               //le reste avec #
                else
                {
                   writer.print(compteur + ": " + ligne);
@@ -103,13 +115,14 @@ public class Jedit {
                ++compteur;
                ligne = reader.readLine();
             }
+            //Sans # -> Écriture
             else
             {
                writer.print(ligne);
                ligne = "\n" + reader.readLine();
             }
          }
-
+         //Fermeture du flux
          writer.close();
       }
       catch (IOException ioe)
@@ -121,7 +134,7 @@ public class Jedit {
 
 
    }
-
+   //Main
    public static void main(String args[])
    {
       Jedit app = new Jedit();
